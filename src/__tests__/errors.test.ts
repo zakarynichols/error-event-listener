24fbf490cdf474240b1849a77b52a722500ba35f
zaknicholsdev <zaknicholsdev1993@gmail.com>
2022-10-02 15:51:23 -0400

    add unit tests

--- src/__tests__/errors.test.ts
+++ src/__tests__/errors.test.ts
@@ -1,7 +1,37 @@
 import { errorEventListener } from "../errors";
 
-it("does not fail", () => {
-  errorEventListener("error", (ev) => {
-    console.log("Uncaught Exception: ", ev);
+it("uncaught exception", () => {
+  const onError = jest.fn(function onError(ev: ErrorEvent) {});
+
+  errorEventListener("error", onError);
+
+  expect(onError).not.toHaveBeenCalled();
+
+  const ev = new Event("error");
+  window.dispatchEvent(ev);
+
+  expect(onError).toHaveBeenCalledTimes(1);
+
+  expect(document.body.firstChild).toBeNull(); // Didn't pass a ui callback. First child is null
+});
+
+it("unhandled rejection", () => {
+  const onError = jest.fn(function onError(ev: PromiseRejectionEvent) {});
+
+  const errorText = "an unhandled rejection occurred";
+  errorEventListener("unhandledrejection", onError, () => {
+    const el = document.createElement("div");
+    el.textContent = errorText;
+    return el;
   });
+
+  expect(onError).not.toHaveBeenCalled();
+
+  const ev = new Event("unhandledrejection");
+  window.dispatchEvent(ev);
+
+  expect(onError).toHaveBeenCalledTimes(1);
+
+  expect(document.body.firstChild).toBeInstanceOf(HTMLDivElement); // Did pass a ui callback. First child is a `div` element
+  expect(document.body.firstChild?.textContent).toBe(errorText);
 });
